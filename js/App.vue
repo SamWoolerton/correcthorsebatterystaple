f16b78d3177098a949e93e31aad4a9a24179feb0
Sam Woolerton <Sam.Woolerton@internal.itpartners.co.nz>
2019-01-15 21:00:22 +1300

    Refactor to improve Vue app structure

---
+++ js/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Main/>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import Main from "./components/Main.vue";
+import Footer from "./components/Footer.vue";
+
+export default {
+  components: { App }
+};
+</script>
--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 import Vue from "vue"
 
-import App from "./components/App.vue"
-import Footer from "./components/Footer.vue"
+import App from "./App.vue"
 
 new Vue({
   el: "#app",
-  components: { App, Footer },
+  components: { App },
 })
